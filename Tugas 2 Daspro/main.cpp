c0491cb91725516c69e084202197bd9a6aa68e26
yayank30 <112909285+yayank30@users.noreply.github.com>
2022-09-17 20:20:45 +0700

    Add files via upload

---
+++ Tugas 2 Daspro/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //integer
+    int u = 18;
+    float t = 155.3;
+    char b = 'M';
+
+    cout << "Umur Saya: " << u;
+    cout << "\nTinggi Saya: " << t;
+    cout << "\nUkuran Baju: " << b;
+
+
+    return 0;
+}
