2efb042374da717c82ada0c501abcfbb82a3a9b4
yayank30 <112909285+yayank30@users.noreply.github.com>
2022-09-25 09:30:32 +0700

    Add files via upload

---
+++ Pertemuan 5/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    float pi;
+    int r;
+
+    pi =3.14;
+    r = 7;
+
+    cout << pi*r*r << endl;
+
+    return 0;
+}
