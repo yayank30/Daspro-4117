f2ef358298a0e61e12110443a20e64645f6ba12f
yayank30 <112909285+yayank30@users.noreply.github.com>
2022-11-23 19:27:22 +0700

    Add files via upload

---
+++ loop5.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Program ini akan berhenti jika Anda menginputkan angka -99" << endl;
+    int x, n, i;
+    for (i = 1; n <= n; i++) {
+        cout << "Masukkan Nilai Anda : ";
+        cin >> x;
+        if (x == -99) {
+            break;
+        }
+    }
+    return 0;
+}
