c2b2caaf6440255a58efb98cba787088e3556a69
yayank30 <112909285+yayank30@users.noreply.github.com>
2022-11-23 19:24:24 +0700

    Add files via upload

---
+++ loop1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i, j;
+    for (i = 100, j = 1; i >= 1, j <= 100;i--, j++) {
+        cout << "i=" << i << " dan " << "j=" << j <<endl;
+    }
+    return 0;
+}
